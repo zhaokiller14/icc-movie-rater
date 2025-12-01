f66a72453fc7e863eed43bbcfdaa63e0a8b79734
zhaokiller14 <63074369+zhaokiller14@users.noreply.github.com>
2025-12-01 07:56:34 +0100

    startup.sh

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/site/wwwroot
 npm install
 npm run build
 node dist/main
--- frontend/startup.sh
+++ frontend/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/site/wwwroot
 npm install
 npm run build --configuration=production
 npx serve -s dist/frontend -l 8080
